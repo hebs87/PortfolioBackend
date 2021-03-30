e142c00f14be30dcd7da6b8a0f44eea3de221ff9
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-03-30 23:13:41 +0100

    Created models and registered in admin site

--- personal/models.py
+++ personal/models.py
@@ -1,3 +1,74 @@
 from django.db import models
 
+
 # Create your models here.
+class PersonalDetails(models.Model):
+    """
+    Stores personal details
+    """
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
+    job_title = models.CharField(max_length=50, blank=False)
+    email = models.EmailField(max_length=50, blank=False)
+    phone = models.CharField(max_length=50, blank=False)
+    address = models.CharField(max_length=50, blank=False)
+    about = models.TextField(max_length=2000, blank=False)
+
+    def __str__(self):
+        return f'{self.first_name.capitalize()} {self.last_name.capitalize()}'
+
+
+class ProfessionalSkills(models.Model):
+    """
+    Stores personal skills
+    """
+    skill = models.CharField(max_length=50, blank=False)
+    percentage_completed = models.PositiveIntegerField(blank=False)
+
+    class Meta:
+        verbose_name = 'Skill'
+        verbose_name_plural = 'Skills'
+
+    def __str__(self):
+        return f'{self.skill} - {self.percentage_completed}%'
+
+
+class WorkExperience(models.Model):
+    """
+    Stores work experience details
+    """
+    company = models.CharField(max_length=50, blank=False)
+    date_from = models.DateField(blank=False)
+    date_to = models.DateField(blank=True)
+    job_title = models.CharField(max_length=50, blank=False)
+    description = models.CharField(max_length=2000, blank=False)
+
+    # TODO - create property of formatted from and to date
+
+    class Meta:
+        verbose_name = 'Job'
+        verbose_name_plural = 'Jobs'
+
+    def __str__(self):
+        return self.company
+
+
+class Education(models.Model):
+    """
+    Stores education details
+    """
+    title = models.CharField(max_length=500, blank=False)
+    qualification_type = models.CharField(max_length=50, blank=False)
+    date = models.DateField(blank=False)
+    awarding_body = models.CharField(max_length=50, blank=False)
+    grade = models.CharField(max_length=20, blank=True)
+    details = models.TextField(max_length=2000, blank=True)
+
+    # TODO - create property of formatted date
+
+    class Meta:
+        verbose_name = 'Subject'
+        verbose_name_plural = 'Subjects'
+
+    def __str__(self):
+        return self.title
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Projects(models.Model):
+    """
+    Stores project details
+    """
+    name = models.CharField(max_length=100, blank=False)
+    description = models.TextField(max_length=500, blank=False)
+    image = models.ImageField(upload_to='projects')
+
+    class Meta:
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
+
+    def __str__(self):
+        return self.name
