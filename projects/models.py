5b81aff6b4ef8f91c8d66570e4b8d3c6852201d9
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-04-01 21:24:46 +0100

    Created Messages model to store message info and added to admin site

--- projects/models.py
+++ projects/models.py
@@ -17,3 +17,16 @@ class Projects(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Messages(models.Model):
+    """
+    Stores details of messages received from contact form
+    """
+    name = models.CharField(max_length=100, blank=False)
+    subject = models.CharField(max_length=200, blank=False)
+    email = models.EmailField(blank=False)
+    message = models.TextField(max_length=2000, blank=False)
+
+    def __str__(self):
+        return f'{self.name} = {self.subject}'
