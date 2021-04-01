ec10789bbeb60e2bafd1ff1710ba8c890ce0ca56
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-04-01 22:05:23 +0100

    Created basic ViewSets for each model

--- personal/views.py
+++ personal/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import PersonalDetails, ProfessionalSkills, WorkExperience, Education
+from .serializers import (
+    PersonalDetailsSerializer, ProfessionalSkillsSerializer,
+    WorkExperienceSerializer, EducationSerializer
+)
+
 
 # Create your views here.
+class PersonalDetailsViewSet(viewsets.ModelViewSet):
+    """
+    A viewset to return PersonalDetails model objects to the frontend
+    Only return the first record, as that is the only record to be rendered in the frontend
+    """
+    queryset = PersonalDetails.objects.first()
+    serializer_class = PersonalDetailsSerializer
+
+
+class ProfessionalSkillsViewSet(viewsets.ModelViewSet):
+    """
+    A viewset to return ProfessionalSkills model objects to the frontend
+    """
+    queryset = ProfessionalSkills.objects.all()
+    serializer_class = ProfessionalSkillsSerializer
+
+
+class WorkExperienceViewSet(viewsets.ModelViewSet):
+    """
+    A viewset to return WorkExperience model objects to the frontend
+    Order by newest to oldest date_from
+    """
+    queryset = WorkExperience.objects.all().order_by('-date_from')
+    serializer_class = WorkExperienceSerializer
+
+
+class EducationViewSet(viewsets.ModelViewSet):
+    """
+    A viewset to return Education model objects to the frontend
+    Order by newest to oldest date_from
+    """
+    queryset = Education.objects.all().order_by('-date_from')
+    serializer_class = EducationSerializer
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Projects, Messages
+from .serializers import ProjectsSerializer, MessagesSerializer
+
 
 # Create your views here.
+class ProjectsViewSet(viewsets.ModelViewSet):
+    """
+    A viewset to return Projects model objects to the frontend
+    """
+    queryset = Projects.objects.all()
+    serializer_class = ProjectsSerializer
+
+
+class MessagesViewSet(viewsets.ModelViewSet):
+    """
+    A viewset to save the messages from the contact form to the Messages model
+    Once saved, an email is sent to me and also the user who completed the contact form
+    """
+    # TODO:
+    #   - Send email functionality - to me and end user
+    #   - Save record
+
+    queryset = Messages.objects.all()
+    serializer_class = MessagesSerializer
